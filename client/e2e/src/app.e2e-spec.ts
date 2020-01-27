92d167bdde5a8ac75163fd907138c7470ad5d478
barak <barman200@gmail.com>
2020-01-27 19:25:46 +0200

    adding server folder

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to site-builder!');
+    expect(page.getTitleText()).toEqual('Welcome to client!');
   });
 });
