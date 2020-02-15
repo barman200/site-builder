43a2bb87feb03f2fed0065f6683cc2ee0d9dd806
barak <barman200@gmail.com>
2020-02-15 23:05:59 +0200

    adding dd

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    
+    loadChildren: () => import('./designer/designer.module').then(m => m.DesignerModule) 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
